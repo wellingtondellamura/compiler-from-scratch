a35710ab33d61ae8913e8468df81f97763940b9b
Wellington Della Mura <della.mura@gmail.com>
2020-03-30 10:10:27 -0300

    basic

---
+++ lectures/simple-translator/grammar.md
@@ -0,0 +1,17 @@
+
+<expr> ::= <term> <rest>
+<rest> ::= + <term> <rest>
+        |  - <term> <rest>
+        |  (nothing)
+<term> ::= 0
+        |  1
+        |  2
+        |  3
+        |  4
+        |  5
+        |  6
+        |  7
+        |  8
+        |  9
+
+
---
+++ lectures/simple-translator/translator-with-tokens.cpp
@@ -0,0 +1,106 @@
+#include <cstdio>
+#include <cctype>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+#define NUMBER 256
+#define SYMBOL 257
+
+using namespace std;
+
+struct token{
+    int type;
+    int value;
+};
+
+string input;
+int pos = -1;
+char peek = ' ';
+token lookahead;
+
+//inputreader
+int nextChar(){
+    if (pos == -1)
+        pos = 0;
+    else if (pos == (int) input.length())
+        return 0;
+    return input[pos++];
+}
+
+//lexer
+token scan(){
+    token t;
+    for (;;peek=nextChar()){
+        if (peek != ' ' && peek != '\t' && peek != '\n')
+            break;
+    }
+    if (isdigit(peek)){
+        int v = 0;
+        do {
+            v = 10 * v + atoi(&peek);
+            peek = nextChar();
+        } while (isdigit(peek));
+        t.type = NUMBER;
+        t.value = v;
+        return t;
+    }
+    t.type = SYMBOL;
+    t.value = peek;
+    peek = ' ';
+    return t;
+}
+
+//parser
+void match(token t){
+    if (lookahead.type == t.type && lookahead.value == t.value){
+        lookahead = scan();
+    }else{
+        printf("\n*** Syntax Error! Values do not match. *** \n");
+        exit(0);
+    }
+}
+
+void term(){
+    if (lookahead.type == NUMBER){
+        printf("%d ", lookahead.value);
+        match(lookahead);
+    }else{
+        printf("\n*** Syntax Error! '%c' it's not a number. ***\n", lookahead.value);
+        exit(0);
+    }
+}
+
+void expr(){
+    term();
+    while(true){
+        if (lookahead.type == SYMBOL){
+            if (lookahead.value == '+'){
+                match(lookahead); 
+                term(); 
+                printf("+ ");
+            }else if (lookahead.value == '-'){
+                match(lookahead); 
+                term(); 
+                printf("- ");
+            }else{
+                return;
+            }
+        }else{
+            return;
+        }
+    }
+}
+
+int main(){
+    printf("Infix: ");
+    getline(cin, input);
+
+    printf("Postfix:");
+    lookahead = scan();
+
+	expr();
+    
+    printf("\n");
+    return 0;
+}
