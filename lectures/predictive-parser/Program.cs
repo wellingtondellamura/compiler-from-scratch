8b81d7b03bfc73f5f226dadd1e3ab7095117c2ec
Wellington Della Mura <della.mura@gmail.com>
2021-10-19 05:59:02 -0300

    Predictive parser

---
+++ lectures/predictive-parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace predictive_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
