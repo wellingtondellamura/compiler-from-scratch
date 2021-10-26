5e825a75c46fb679a83b64f581f94a762cda6b7d
Wellington Della Mura <della.mura@gmail.com>
2021-10-26 11:09:26 -0300

    Integração com outras linguagens de programação

---
+++ lectures/intro-antlr/csharp_project/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
+using Grammar;
+namespace csharp_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "1  +2 ;";
+            AntlrInputStream inputStream = new AntlrInputStream(text);
+            SimpleGrammarLexer lexer = new SimpleGrammarLexer(inputStream);
+            // var tokens = lexer.GetAllTokens();
+            // foreach (var t in tokens)
+            // {
+            //     Console.WriteLine(t);
+            // }
+            BufferedTokenStream tokenStream = new BufferedTokenStream(lexer);
+            SimpleGrammarParser parser = new SimpleGrammarParser(tokenStream);
+            
+            parser.AddParseListener(new MyListener());
+
+            var prog = parser.prog();
+        }
+    }
+}
